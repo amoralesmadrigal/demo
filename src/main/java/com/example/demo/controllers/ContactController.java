688981f2d4bb0871ac4850c77acadecacd21687f
Adan <kraken01@gmail.com>
2024-06-11 17:07:48 +0200

    se agrega profile

--- src/main/java/com/example/demo/controllers/ContactController.java
+++ src/main/java/com/example/demo/controllers/ContactController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +12,6 @@ import com.example.demo.services.ContactService;
 
 @RestController
 @RequestMapping("/api/contacts")
-//@CrossOrigin(origins = "http://localhost:4200")
 public class ContactController {
 
 	@Autowired
