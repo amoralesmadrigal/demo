0be55b26d9e4d2ad5683f4bf91bcd1678bf1aa12
Adan <kraken01@gmail.com>
2024-11-25 22:02:26 +0100

    se modifica seguridad y dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,31 @@
 #
 # Build stage
 #
-FROM maven:3.8.3-openjdk-17 AS build
+FROM maven:3.8.3-openjdk-17-slim AS build
 COPY . .
 RUN mvn clean package -DskipTests
 
 #
 # Package stage
 #
-FROM openjdk:17-jdk-slim
-ARG JAR_FILE=target/*.jar
+FROM eclipse-temurin:17-jre-alpine
+#ARG JAR_FILE=target/*.jar
 COPY --from=build ./target/demo-0.0.1-SNAPSHOT.jar app.jar
 
+# Ejecuta con opciones de JVM optimizadas
+CMD java \
+    -XX:+UseContainerSupport \
+    -XX:MaxRAMPercentage=70.0 \
+    -XX:InitialRAMPercentage=30.0 \
+    -XX:MinRAMPercentage=15.0 \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:+UseG1GC \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:ParallelGCThreads=1 \
+    -Djava.security.egd=file:/dev/./urandom \
+    -jar app.jar
+	
 # ENV PORT=8080
 EXPOSE 8080
 
-ENTRYPOINT [ "java", "-jar", "/app.jar"]
+#ENTRYPOINT [ "java", "-jar", "/app.jar"]
---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Value("${allowed.origin}")
+    private String allowedOrigin;
+	
+	 @Bean
+	    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configura CORS
+            .csrf(csrf -> csrf.disable()) // Deshabilita CSRF si no lo necesitas (por ejemplo, en APIs REST)
+	            .headers(headers -> headers
+	                .xssProtection(xss -> xss.disable() // Activa X-XSS-Protection con modo "block"
+	                )
+	                .contentTypeOptions(contentType -> contentType.disable() // Configura X-Content-Type-Options: nosniff
+	                )
+	                .frameOptions(frame -> frame
+	                    .sameOrigin() // Configura X-Frame-Options: SAMEORIGIN
+	                )
+	                .contentSecurityPolicy(csp -> csp
+	                    .policyDirectives("default-src 'self'") // Configura Content-Security-Policy
+	                )
+	            );
+	        return http.build();
+	    }
+	 
+	 /**
+     * Configuración personalizada de CORS
+     */
+    @Bean
+   CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigin)); // Dominios permitidos
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS")); // Métodos permitidos
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization")); // Encabezados permitidos
+        configuration.setMaxAge(3600L); // Cache de preflight por 3600 segundos (1 hora)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplica a todas las rutas
+        return source;
+    }
+}
